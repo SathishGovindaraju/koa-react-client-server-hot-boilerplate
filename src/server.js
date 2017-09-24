fdaef7ea443c8c4814bc5bc89c6037b0396a8904
Sathish Govindaraju <sathish.govindaraju@sky.uk>
2017-09-24 10:50:12 +0100

    Working properly with koa-react-router

--- src/server.js
+++ src/server.js
@@ -1,27 +1,26 @@
-// require('babel-polyfill');
-const React = require('react');
+import React from 'react';
+import KoaReactRouter from 'koa-react-router';
 const renderToString = require('react-dom/server').renderToString;
 import App from './component/App.jsx';
 import {StaticRouter} from 'react-router-dom';
 
-module.exports = function serverRenderer({ clientStats, serverStats, foo }) {
-    return (ctx, next) => {
-        ctx.status = 200;
-        ctx.body = `
-            <!doctype html>
-            <html>
-            <head>
-                <title>${foo}</title>
-            </head>
-            <body>
-                <div id="root">${renderToString(
-                  <StaticRouter location={ctx.req.url} context={{}}>
-                    <App />
-                  </ StaticRouter>
-                )}</div>
-                <script src="/client.js"></script>
-            </body>
-            </html>
-        `;
-    };
-}
+const Container = props => (
+    <html>
+    <head>
+        <title>Sathish</title>
+    </head>
+    <body>
+        <div id="root">
+            {props.children}
+        </div>
+        <script src="/client.js"></script>
+    </body>
+    </html>
+);
+
+export default () => KoaReactRouter({
+    App,
+    onError: (ctx, err) => console.log('I Have failed!!!! \n', err),
+    onRedirect: (ctx, redirect) => console.log('I have redirected!'),
+    onRender: (ctx, next) => ({ Container: Container })
+});
